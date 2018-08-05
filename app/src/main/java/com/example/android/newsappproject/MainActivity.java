afa77607639c2fa30d52486e78e4a9b2af6119a7
Erick <boymorotoi@gmial.com>
2018-08-05 17:02:57 -0500

    new main activity

--- app/src/main/java/com/example/android/newsappproject/MainActivity.java
+++ app/src/main/java/com/example/android/newsappproject/MainActivity.java
@@ -1,33 +1,31 @@
 package com.example.android.newsappproject;
-
 import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Intent;
-import android.content.Loader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.Handler;
+import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity implements android.support.v4.app.LoaderManager.LoaderCallbacks<List<Technology>> {
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<Technology>> {
      // Log messages tag
      private  static final String Log_Tag = MainActivity.class.getName();
 
      // news data
-     private static final String USGS_REGUEST_URL  =
-             "https://content.guardianapis.com/global-development/2018/jul/31/mps-accuse-aid-groups-of-abject-failure-in-tackling-sexual-abuse";
+     private static final String USGS_REQUEST_URL  =
+             "https://content.guardianapis.com/global-development/2018/jul/31/mps-accuse-aid-groups-of-abject-failure-in-tackling-sexual-abuse&api-key=";
     // the static value
      private static final int TECHNOLOGY_LOADER_ID = 1;
     // list of articles
@@ -102,35 +100,37 @@ public class MainActivity extends AppCompatActivity implements android.support.v
             loaderManager.initLoader(TECHNOLOGY_LOADER_ID, null, this);
         } else {
 
-            mProgressBar = (ProgressBar) findViewById(R.id.progress_rate);
+            mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
             mProgressBar.setVisibility(View.GONE);
 
             mEmptyState.setText(R.string.No_internet_found);
         }
     }
     @Override
-    public Loader<List<Technology>> onCreateLoader(int i,Bundle bundle){
-        return new TechnologyLoader(this,USGS_REGUEST_URL);
+    public android.content.Loader<List<Technology>> onCreateLoader(int i, Bundle bundle){
+        return new TechnologyLoader(this,USGS_REQUEST_URL);
     }
 
-    @Override
-    public void onLoadFinished(Loader<List<Technology>> loader, List<Technology> technologies) {
 
-        mProgressBar = (ProgressBar) findViewById(R.id.progress_rate);
+
+
+    @Override
+    public void onLoadFinished(android.content.Loader<List<Technology>> loader, List<Technology> data) {
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         mProgressBar.setVisibility(View.GONE);
 
         mEmptyState.setText(R.string.No_news_found);
 
         mTechnologyAdapter.clear();
 
-        if (technologies != null && !technologies.isEmpty()) {
-            mTechnologyAdapter.addAll(technologies);
+        if (data != null && !data.isEmpty()) {
+            mTechnologyAdapter.addAll(data);
         }
 
     }
-
     @Override
-    public void onLoaderReset(Loader<List<Technology>> loader){
+    public void onLoaderReset(android.content.Loader<List<Technology>> loader){
         Log.i(Log_Tag,"onLoaderReset() called");
         mTechnologyAdapter.clear();
 
